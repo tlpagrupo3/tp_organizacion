118cdcc5d4093409f6d50ee8991c35ad1a03c631
Dracio1 <dracio39@gmail.com>
2020-10-27 18:09:16 -0300

    chat de prueba

---
+++ index.js
@@ -0,0 +1,24 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+  });
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
+io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
+
+io.on('connection', (socket) => {
+    socket.broadcast.emit('hi');
+  });
+
