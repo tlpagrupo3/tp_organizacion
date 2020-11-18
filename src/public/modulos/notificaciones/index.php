4488176a448441e53cf797ff2d901e0592c9f127
Noelia-lopez <akibanoelia@gmail.com>
2020-11-18 17:33:04 -0300

    noticias

--- src/public/modulos/notificaciones/index.php
+++ src/public/modulos/notificaciones/index.php
@@ -13,22 +13,17 @@ require "../../templates/navbar.php";
 
 <!-- Main content: shift it to the right by 250 pixels when the sidebar is visible -->
 <div class="w3-main" style="margin-left:250px">
-
   <div class="w3-row w3-padding-64">
     <div class="w3-twothird w3-container">
       <h1 class="w3-text-teal">Informacion del sistema</h1>
-      <p></p>
+      <p>En esta seccion se pueden autorizar o denegar las noticias que estan sujetas a aprobacion para el portal web</p>
     </div>
   </div>
 <div>
   <h3>Autorizaciones pendientes</h3>
   <div id='noticias'>
-
   </div>
 </div>
-
-  
-  <!-- Pagination -->
       <!-- Pagination -->
       <div class="w3-center w3-padding-32">
     <div class="w3-bar">
@@ -44,11 +39,9 @@ require "../../templates/navbar.php";
 <!-- END MAIN -->
 </div><script src="../../js/elementosHTML.js"></script>
 <script src="../../js/notificaciones.js"></script>
+
 <?php
-
-
 require "../../templates/footer.php";
-
 ?>  
 </body>
 </html>
