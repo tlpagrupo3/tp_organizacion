9196a9ace05017a87f7cdebc93c3367173481aba
Dracio1 <dracio39@gmail.com>
2020-11-18 17:31:50 -0300

    eliminar noticias

--- src/public/js/notificaciones.js
+++ src/public/js/notificaciones.js
@@ -5,11 +5,11 @@ fetch('../../php/consultas/consulta_noticias_sin_autorizar.php')
     noticias.forEach(noticia => {
         news+=
         `        
-        <div id='noticia${noticia.id_noticia}' style='max-width:500px;max-height: 500px;
+        <div id='noticia${noticia.id_noticias}' style='max-width:500px;max-height: 500px;
         overflow: auto;'>
             <div class='fecha'>${noticia.fecha}</div>
             <div class='volanta'>${noticia.volanta}</div>
-            <div class='titutar'>${noticia.titular}</div>
+            <div class='titular'>${noticia.titular}</div>
             <div class='imagen' style='max-width=200px;'><img src='http://localhost/sadop/src/public/${noticia.imagen}' ></div>
             <div class='epigrafe'>${noticia.epigrafe}</div>
             <div class='copete'>${noticia.copete}</div>
@@ -17,8 +17,12 @@ fetch('../../php/consultas/consulta_noticias_sin_autorizar.php')
             
         </div>
         <div>
-            <form action=''>
-                <select>
+            <h3>Autorizar Noticia</h3>
+            <form action='../../php/noticia_abm.php' method='POST'>
+                <input type='text' value='${noticia.id_noticias}' hidden name='id_noticias'>
+                <input type='text' value='autorizar' hidden name='accion'>
+                <select name='autorizacion'>
+                    
                     <option selected disabled>Seleccione una opción</option>
                     <option value='n'>No autorizar</option>
                     <option value='s'>Autorizar</option>
@@ -26,6 +30,18 @@ fetch('../../php/consultas/consulta_noticias_sin_autorizar.php')
                 <input type='submit' value='Enviar'>
             </form>
         </div>
+        <div>
+        <h3>Eliminar Noticia</h3>
+        <form action='../../php/noticia_abm.php' method='POST'>
+            <input type='text' value='eliminar' hidden name='accion'>
+            <select name='id_noticias'>                
+                <option selected disabled>Seleccione una opción</option>
+                <option value='n'>No Eliminar</option>
+                <option value='${noticia.id_noticias}'>Eliminar</option>
+            </select>
+            <input type='submit' value='Enviar'>
+        </form>
+    </div>
         `
     });
     document.querySelector('#noticias').innerHTML= news
