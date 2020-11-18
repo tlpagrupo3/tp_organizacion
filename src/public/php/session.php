4db1c53827240fde0b0673d3cc9d75ccd86ab27f
Noelia-lopez <akibanoelia@gmail.com>
2020-11-17 22:31:57 -0300

    estilo

--- src/public/php/session.php
+++ src/public/php/session.php
@@ -2,7 +2,7 @@
 session_start();
 
 if (!isset($_SESSION['nombre']) || !isset($_SESSION['apellido']) || !isset($_SESSION['codigo_acceso']) || !isset($_SESSION['id_tipo_genero'])) {
-    header('Location: /src/public/modulos/login/');
+    header('Location: sadop/src/public/modulos/login/');
 }
 
 ?>
