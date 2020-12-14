ccc86c80a2bd033b5212a7f69f3ee14b92c24c8f
Dracio1 <dracio39@gmail.com>
2020-12-14 06:42:47 -0300

    actividades

--- src/public/php/consultas/consulta_actividades.php
+++ src/public/php/consultas/consulta_actividades.php
@@ -1,9 +1,11 @@
 <?php
 require '../../bd/conexion.php';
-
-$sql="Select * from publicaciones.actividades join miembros.usuarios on (id_usuario=id_usuarios) left join miembros.miembros using (id_miembros) where autorizacion ='s' order by fecha desc";
+$fecha= date('Y-m-d');
+print_r($fecha);
+$sql="Select * from publicaciones.actividades join miembros.usuarios on (id_usuario=id_usuarios) left join miembros.miembros using (id_miembros) where autorizacion ='s' and fecha > '".$fecha."'::date order by fecha desc";
 $stmt=$conexion->query($sql);
 $actividades=$stmt->fetchAll(PDO::FETCH_OBJ);
 
 echo json_encode($actividades);
+// ".$añoActual."-".$mesActual."-".$diaActual."
 ?>
