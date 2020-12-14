bbf3bbf04efb285213c735f73b89c60d9597aab0
Dracio1 <dracio39@gmail.com>
2020-12-14 06:44:32 -0300

    correccion

--- src/public/php/consultas/consulta_actividades.php
+++ src/public/php/consultas/consulta_actividades.php
@@ -1,7 +1,6 @@
 <?php
 require '../../bd/conexion.php';
 $fecha= date('Y-m-d');
-print_r($fecha);
 $sql="Select * from publicaciones.actividades join miembros.usuarios on (id_usuario=id_usuarios) left join miembros.miembros using (id_miembros) where autorizacion ='s' and fecha > '".$fecha."'::date order by fecha desc";
 $stmt=$conexion->query($sql);
 $actividades=$stmt->fetchAll(PDO::FETCH_OBJ);
